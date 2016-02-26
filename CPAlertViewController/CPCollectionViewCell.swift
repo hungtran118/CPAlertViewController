89c6e2a97f26b04723bcfd4be634fe2cae477c0d
Wei Zhao <cp3hnu@gmail.com>
2016-02-26 22:43:48 +0800

    submit

---
+++ CPAlertViewController/CPCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CPCollectionViewCell.swift
+//  CPAlertViewController
+//
+//  Created by ZhaoWei on 15/12/10.
+//  Copyright © 2015年 CP3HNU. All rights reserved.
+//
+
+import UIKit
+
+class CPCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var detailLabel: UILabel!
+}
--- CPAlertViewController/ViewController.swift
+++ CPAlertViewController/ViewController.swift
@@ -8,18 +8,203 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CPCollectionViewCell
+    
+        var title: String = "Title"
+        var detail: String = "Detail"
+        switch indexPath.row {
+        case 0:
+            title = "Success"
+            detail = "Normal"
+        case 1:
+            title = "Error"
+            detail = "Normal"
+        case 2:
+            title = "Warning"
+            detail = "Normal"
+        case 3:
+            title = "Info"
+            detail = "Normal"
+        case 4:
+            title = "None Image"
+            detail = "Normal"
+        case 5:
+            title = "Custom image"
+            detail = "Normal"
+        case 6:
+            title = "Success"
+            detail = "Two buttons"
+        case 7:
+            title = "Success"
+            detail = "Long title"
+        case 8:
+            title = "Success"
+            detail = "Very long title"
+        case 9:
+            title = "Success"
+            detail = "Long message"
+        case 10:
+            title = "Success"
+            detail = "Very long message"
+        case 11:
+            title = "Success"
+            detail = "No title"
+        case 12:
+            title = "Success"
+            detail = "No message"
+        //Test
+//        case 13:
+//            title = "Success"
+//            detail = "No title and message"
+//        case 14:
+//            title = "None"
+//            detail = "No image, title and message"
+        default:
+            break
+        }
+        
+        cell.titleLabel.text = title
+        cell.detailLabel.text = detail
+       
+        return cell;
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        switch indexPath.row {
+        case 0:
+            show1()
+        case 1:
+            show2()
+        case 2:
+            show3()
+        case 3:
+            show4()
+        case 4:
+            show5()
+        case 5:
+            show6()
+        case 6:
+            show7()
+        case 7:
+            show8()
+        case 8:
+            show9()
+        case 9:
+            show10()
+        case 10:
+            show11()
+        case 11:
+            show12()
+        case 12:
+            show13()
+        //Test
+//        case 13:
+//            show14()
+//        case 14:
+//            show15()
+        default:
+            break
+        }
+    }
+    
+    func show1() {
+        CPAlertViewController().showSuccess(title:"Title", message: "Message")
+    }
+    
+    func show2() {
+        CPAlertViewController().showError(title:"Title", message: "Message")
+    }
+    
+    func show3() {
+        CPAlertViewController().showWarning(title:"Title", message: "Message")
+    }
+    
+    func show4() {
+        CPAlertViewController().showInfo(title:"Title", message: "Message")
+    }
+    
+    func show5() {
+        CPAlertViewController().show(title: "Title", message: "Message", style: .None)
+    }
+    
+    func show6() {
+        CPAlertViewController().show(title: "Title", message: "Message", style: .CustomImage(imageName: "thumb"))
+    }
+    
+    func show7() {
+        
+        CPAlertViewController().showSuccess(title: "Title", message: "Message", buttonTitle: "确定", otherButtonTitle: "取消", action:{(buttonIndex: Int) -> Void in
+            print("Clicked at index = ", buttonIndex)
+        })
+    }
+    
+    func show8() {
+        CPAlertViewController().showSuccess(title:"Titletitletitletitletitletitletitletitletitletitletitle", message: "Message")
+    }
+    
+    func show9() {
+        
+        CPAlertViewController().showSuccess(title:"Titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle", message: "Message")
+    }
+    
+    func show10() {
+        
+        CPAlertViewController().showSuccess(title:"Title", message: "MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage")
+    }
+    
+    func show11() {
+        
+        CPAlertViewController().showSuccess(title:"Title", message: "MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage")
+    }
+    
+    func show12() {
+        
+        CPAlertViewController().showSuccess(title: nil, message: "Message")
+    }
+    
+    func show13() {
+        
+        CPAlertViewController().showSuccess(title: "Title", message: nil)
+    }
+    
+    //test
+//    func show14() {
+//        
+//        CPAlertViewController().showSuccess(title: nil, message: nil)
+//    }
+//    
+//    func show15() {
+//        
+//        CPAlertViewController().show(title: nil, message: nil, buttonTitle: "确定", otherButtonTitle: nil, action:{(buttonIndex: Int) -> Void in
+//            print("Clicked at index = ", buttonIndex)
+//        })
+//    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    
+    }
 }
 
